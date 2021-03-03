96dfb7fefe4cad43cf07bce397f2d2fdfe61c047
Nick LaMonica <nila2238@lasp.colorado.edu>
2021-03-02 21:06:27 -0700

    mod build.gradle to solve linking errors, add test for correctness

--- src/test/java/parspice/ReflectionTest.java
+++ src/test/java/parspice/ReflectionTest.java
@@ -6,20 +6,46 @@ import spice.basic.SpiceErrorException;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 
 public class ReflectionTest {
 
-    @Test // test native call speed
+
+    @Test // test native call for correctness
     void nativeTest() {
-        // init
+        System.loadLibrary("JNISpice");
+        double[][] m1 = {{1,2,3}, {4,5,6}, {7,8,9}};
+        double[][] m2 = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+        try {
+            System.out.println(Arrays.deepToString(CSPICE.mxm(m1, m2)));
+        } catch (SpiceErrorException e) {
+            System.out.println("error");
+        }
+    }
+
+    @Test // test reflection for correctness
+    void reflectionTest() {
+        double[][] m1 = {{1,2,3}, {4,5,6}, {7,8,9}};
+        double[][] m2 = {{1,2,3}, {4,5,6}, {7,8,9}};
+        try {
+            Method method = CSPICE.class.getDeclaredMethod("mxm", double[][].class, double[][].class);
+            System.out.println(Arrays.deepToString((double[][])method.invoke(null, m1, m2)));
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            System.out.println("error");
+        }
+    }
+
+    @Test // test native call speed
+    void nativeTestTime() {
         double[][] m1 = {{0,0,0},{0,0,0}, {0,0,0}};
         double[][] m2 = {{0,0,0},{0,0,0}, {0,0,0}};
         long startTime;
         long stopTime;
 
         startTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100000; i++) {
             try {
                 CSPICE.mxm(m1, m2);
             } catch (SpiceErrorException e) {
@@ -27,28 +53,27 @@ public class ReflectionTest {
             }
         }
         stopTime = System.currentTimeMillis();
-        System.out.println(stopTime-startTime);
+        System.out.printf("Native call = %d millis \n", stopTime-startTime);
     }
 
     @Test // test reflection on every iteration speed
     void reflectionTestEveryTime() {
-        // init
         double[][] m1 = {{0,0,0},{0,0,0}, {0,0,0}};
         double[][] m2 = {{0,0,0},{0,0,0}, {0,0,0}};
         long startTime;
         long stopTime;
 
         startTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100000; i++) {
             try {
-                Method method = CSPICE.class.getDeclaredMethod("mxm");
-                method.invoke(null, m1, m2);
+                Method method = CSPICE.class.getDeclaredMethod("mxm", double[][].class, double[][].class);
+                double[][] res = (double[][])method.invoke(null, m1, m2);
             } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
                 System.out.println("error");
             }
         }
         stopTime = System.currentTimeMillis();
-        System.out.println(stopTime-startTime);
+        System.out.printf("Using reflection every iteration = %d millis\n", stopTime-startTime);
     }
 
     @Test // test only one reflexive call speed
@@ -60,10 +85,10 @@ public class ReflectionTest {
 
         startTime = System.currentTimeMillis();
         try {
-            Method method = CSPICE.class.getDeclaredMethod("mxm");
-            for (int i = 0; i < 1000; i++) {
+            Method method = CSPICE.class.getDeclaredMethod("mxm", double[][].class, double[][].class);
+            for (int i = 0; i < 100000; i++) {
                 try {
-                    method.invoke(null, m1, m2);
+                    double[][] res = (double[][])method.invoke(null, m1, m2);
                 } catch ( IllegalAccessException | InvocationTargetException e) {
                     System.out.println("error");
                 }
@@ -72,7 +97,6 @@ public class ReflectionTest {
             System.out.println("error");
         }
         stopTime = System.currentTimeMillis();
-        System.out.println(stopTime-startTime);
+        System.out.printf("Using reflection only once = %d millis\n", stopTime-startTime);
     }
-
 }
