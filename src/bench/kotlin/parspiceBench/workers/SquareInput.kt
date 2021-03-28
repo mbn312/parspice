9b6a94b109014ff687320c1ca23bbebfc5f2f865
Joel Courtney <joel.e.courtney@gmail.com>
2021-03-28 11:47:04 -0600

    change to double, not array

--- src/bench/kotlin/parspiceBench/workers/SquareInput.kt
+++ src/bench/kotlin/parspiceBench/workers/SquareInput.kt
@@ -1,13 +1,12 @@
 package parspiceBench.workers
 
-import parspice.sender.DoubleArraySender
-import parspice.sender.Sender
+import parspice.sender.DoubleSender
 import parspice.worker.OWorker
 import parspiceBench.BenchWorker
 import kotlin.math.pow
 
-class SquareInput : BenchWorker<DoubleArray>() {
-    override val bytes = 2*Double.SIZE_BYTES
+class SquareInput : BenchWorker<Double>() {
+    override val bytes = Double.SIZE_BYTES
     override val iterations: Map<Int, IntArray> = mapOf(
         2 to intArrayOf(1000, 100000),
         4 to intArrayOf(1000, 10000, 100000, 1000000),
@@ -15,13 +14,9 @@ class SquareInput : BenchWorker<DoubleArray>() {
         8 to intArrayOf(100, 1000, 100000, 1000000, 10000000, 50000000)
     )
 
-    override fun getOutputSender(): Sender<DoubleArray> {
-        return DoubleArraySender(2)
-    }
+    override fun getOutputSender() = DoubleSender()
 
-    override fun task(i: Int): DoubleArray {
-        return doubleArrayOf(i.toDouble(), i.toDouble().pow(2))
-    }
+    override fun task(i: Int) = i.toDouble().pow(2)
 
     companion object {
         @JvmStatic
