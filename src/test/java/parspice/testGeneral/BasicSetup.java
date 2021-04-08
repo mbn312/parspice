1f71d18b3c51b76dc02d491a89e705d3a2950ffe
mattcohen1326 <maco2574@colorado.edu>
2021-04-08 10:17:00 -0400

    Basic Setup Test Added

---
+++ src/test/java/parspice/testGeneral/BasicSetup.java
@@ -0,0 +1,35 @@
+package parspice.testGeneral;
+
+import org.junit.jupiter.api.TestInstance;
+import parspice.sender.DoubleArraySender;
+import parspice.worker.OWorker;
+
+
+import org.junit.jupiter.api.Test;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BasicSetup extends OWorker<double[]> {
+    public BasicSetup() {
+        super(new DoubleArraySender(3));
+    }
+
+    @Override
+    public void setup() throws Exception {
+        System.load(System.getenv("JNISPICE_ROOT") + "/lib/JNISpice.dll");
+    }
+
+    @Override
+    public double[] task(int i) throws Exception {
+        double ret[] = {1.1,2.2};
+        return ret;
+    }
+
+    @Test
+    public void testSetup(){
+        try {
+            this.setup();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
